442a61a0b940502b374b642e795984a1b0b5e1f6
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-03 19:40:38 +0700

    Tidy up header section for StudentDashboard component

--- client/src/components/dashboard/StudentDashboard.js
+++ client/src/components/dashboard/StudentDashboard.js
@@ -1,20 +1,29 @@
+import { useState } from 'react'
 import { FaFilter } from 'react-icons/fa'
+import { AiOutlineSortAscending, AiOutlineSortDescending, AiOutlineSearch } from 'react-icons/ai'
 
 const StudentDashboard =  () => {
+  const [isOpenFilter, setIsOpenFilter] = useState(false)
+
   return (
     <div className='studentDashboard'>
       <div className="studentDashboard__header">
-        <input type='text' placeholder='Search class ...' />
+        <div className='studentDashboard__search'>
+          <input type='text' placeholder='Search class ...' />
+          <AiOutlineSearch />
+        </div>
         <div className='studentDashboard__filter'>
-          <div className='studentDashboard__filter--btn'>
+          <div className='studentDashboard__filter--btn' onClick={() => setIsOpenFilter(!isOpenFilter)}>
             <FaFilter />
             <p>Filter</p>
           </div>
-          <div className="studentDashboard__filter--dropdown">
+          <div className={`studentDashboard__filter--dropdown ${isOpenFilter ? 'active' : undefined}`}>
             <div>
+              <AiOutlineSortDescending />
               Oldest to newest
             </div>
             <div>
+              <AiOutlineSortAscending />
               Newest to oldest
             </div>
           </div>
