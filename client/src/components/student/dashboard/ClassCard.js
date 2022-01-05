40a682767ba84f8f26135fd86833464779af7090
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-05 22:21:04 +0700

    Create dynamic ClassCard component based on user role and place it at teacher dashboard

--- client/src/components/student/dashboard/ClassCard.js
+++ client/src/components/student/dashboard/ClassCard.js
@@ -1,10 +1,25 @@
-const ClassCard = ({title, description, instructor}) => {
+const ClassCard = ({isTeacher, title, description, instructor, totalQuiz, totalStudent}) => {
   return (
     <div className='classCard'>
       <h2>{title}</h2>
       <p>{description}</p>
-      <p>Instructor: {instructor}</p>
-      <button>Expand</button>
+
+      {
+        isTeacher
+        ? (
+          <div className='classCard__teacherInfo'>
+            <p>Total Quiz : {totalQuiz}</p>
+            <p>Total Student : {totalStudent}</p>
+          </div>
+        )
+        : <p>Instructor: {instructor}</p>
+      }
+      
+      {
+        isTeacher
+        ? <button>More &gt;</button>
+        : <button>Expand</button>
+      }
     </div>
   )
 }
--- client/src/components/teacher/dashboard/TeacherDashboard.js
+++ client/src/components/teacher/dashboard/TeacherDashboard.js
@@ -1,4 +1,6 @@
+import { AiOutlineSearch } from 'react-icons/ai'
 import InfoCard from './../../global/InfoCard'
+import ClassCard from './../../student/dashboard/ClassCard'
 
 const TeacherDashboard = () => {
   return (
@@ -7,6 +9,24 @@ const TeacherDashboard = () => {
         <InfoCard />
         <InfoCard />
         <InfoCard />
+      </div>
+      <div className='teacherDashboard__body'>
+        <div className="teacherDashboard__body--title">
+          <h2>Your Classes</h2>
+          <div>
+            <input type='text' placeholder='Search class ...' />
+            <AiOutlineSearch />
+          </div>
+        </div>
+        <div className='teacherDashboard__classList'>
+          <ClassCard
+            isTeacher={true}
+            title='Class Title Goes Here'
+            description='Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+            totalQuiz={5}
+            totalStudent={7}
+          />
+        </div>
       </div>
     </div>
   )
