f8be5fdc2beb468ed775649ed108f32bdbc35471
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-05 23:51:11 +0700

    Add search box at PeopleList and QuizList component

--- client/src/components/teacher/people/PeopleList.js
+++ client/src/components/teacher/people/PeopleList.js
@@ -1,8 +1,13 @@
+import { AiOutlineSearch } from 'react-icons/ai'
 import PeopleCard from './PeopleCard'
 
 const PeopleList = () => {
   return (
     <div>
+      <div className='searchInput'>
+        <input type='text' placeholder='Search people ...' />
+        <AiOutlineSearch />
+      </div>
       <PeopleCard />
       <PeopleCard />
       <PeopleCard />
--- client/src/components/teacher/quizes/QuizList.js
+++ client/src/components/teacher/quizes/QuizList.js
@@ -1,8 +1,13 @@
+import { AiOutlineSearch } from 'react-icons/ai'
 import QuizCard from './QuizCard'
 
 const QuizList = () => {
   return (
     <div>
+      <div className='searchInput'>
+        <input type='text' placeholder='Search quiz ...' />
+        <AiOutlineSearch />
+      </div>
       <QuizCard />
       <QuizCard />
       <QuizCard />
