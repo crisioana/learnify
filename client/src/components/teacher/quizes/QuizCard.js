cd444cd5106d45a4d7e19bd124b8ca13b069344b
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-17 16:04:45 +0700

    Setup view submission modal at QuizList component

--- client/src/components/teacher/quizes/QuizCard.js
+++ client/src/components/teacher/quizes/QuizCard.js
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { FaEye, FaTrash, FaEdit } from 'react-icons/fa'
 import { changeQuizStatus, deleteQuiz } from './../../../redux/actions/quizActions'
+import SubmissionModal from './../submission/SubmissionModal'
 
 const QuizCard = ({id, title, status}) => {
+  const [viewSubmission, setViewSubmission] = useState(false) 
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { auth } = useSelector(state => state)
@@ -27,22 +31,30 @@ const QuizCard = ({id, title, status}) => {
   }
 
   return (
-    <div className='teacherQuizCard'>
-      <div className='teacherQuizCard__left'>
-        <h4>{title}</h4>
+    <>
+      <div className='teacherQuizCard'>
+        <div className='teacherQuizCard__left'>
+          <h4>{title}</h4>
+        </div>
+        <div className='teacherQuizCard__right'>
+          <FaEye onClick={() => setViewSubmission(true)} />
+          <FaEdit onClick={handleEdit} />
+          <FaTrash onClick={handleDelete} />
+          <p
+            onClick={handleChangeStatus}
+            className={status === 'Open' ? 'open' : 'close'}
+          >
+            {status === 'Open' ? 'Open' : 'Close'}
+          </p>
+        </div>
       </div>
-      <div className='teacherQuizCard__right'>
-        <FaEye />
-        <FaEdit onClick={handleEdit} />
-        <FaTrash onClick={handleDelete} />
-        <p
-          onClick={handleChangeStatus}
-          className={status === 'Open' ? 'open' : 'close'}
-        >
-          {status === 'Open' ? 'Open' : 'Close'}
-        </p>
-      </div>
-    </div>
+
+      <SubmissionModal
+        title={title}
+        viewSubmission={viewSubmission}
+        setViewSubmission={setViewSubmission}
+      />
+    </>
   )
 }
 
---
+++ client/src/components/teacher/submission/SubmissionCard.js
@@ -0,0 +1,12 @@
+import { FaEye } from 'react-icons/fa'
+
+const SubmissionCard = () => {
+  return (
+    <div className='submissionCard'>
+      <h4>Student Name</h4>
+      <FaEye />
+    </div>
+  )
+}
+
+export default SubmissionCard
